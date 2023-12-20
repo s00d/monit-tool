373c52db036c0d8f410011e053e5b7989855efb6
Pavel Kuzmin <Virus191288@gmail.com>
2023-12-20 15:41:09 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,85 @@
+# Process Monitor in Rust
+
+This Rust application provides a simple interface to monitor CPU and memory usage of processes running on your system. It allows users to filter and select a process from the list and displays real-time CPU and memory usage using colored graphs.
+
+## Features
+
+- **Process Filtering**: Filter processes by name.
+- **Interactive Selection**: Choose a process to monitor from a filtered list.
+- **Real-time Monitoring**: View real-time CPU and memory usage of the selected process.
+- **Graphical Display**: CPU and memory usage are displayed as colored lines on a graph.
+
+## Requirements
+
+- Rust programming environment.
+- Dependencies: `sysinfo`, `dialoguer`, `rgb`, `textplots`.
+
+## Installation
+
+1. Clone the repository:
+   ```
+   git clone https://github.com/s00d/monit-tool
+   ```
+2. Navigate to the directory:
+   ```
+   cd ./monit-tool
+   ```
+3. Build the project:
+   ```
+   cargo build --release
+   ```
+
+## crates.io
+
+Before installing the `monit-tool` package, you need to install Rust. Rust is a programming language that the package is built with. Here are the steps to install Rust:
+
+1. Open a terminal or command prompt.
+
+2. Visit the official Rust website at [https://www.rust-lang.org/](https://www.rust-lang.org/).
+
+3. Follow the instructions on the website to download and install Rust for your operating system.
+
+4. After the installation is complete, verify that Rust is installed correctly by running the following command in your terminal:
+
+```shell
+rustc --version
+```
+
+You can install the `monit-tool` package using the `cargo` utility. Make sure you have Rust compiler and `cargo` tool installed.
+
+1. Open a terminal or command prompt.
+
+2. Run the following command to install the package:
+
+```shell
+cargo install monit-tool
+```
+
+## Usage
+
+1. Run the program:
+   ```
+   monit-tool
+   ```
+2. Enter a filter to search for a specific process or leave it blank to list all processes.
+3. Select a process from the list to monitor.
+4. The program will display real-time CPU and memory usage on a graphical chart.
+
+## Customization
+
+- Modify the graph dimensions or the refresh rate in the main loop for different display preferences.
+
+## Contributing
+
+Contributions, issues, and feature requests are welcome. Feel free to check [issues page](link-to-issues-page) if you want to contribute.
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+## Acknowledgements
+
+- [sysinfo](https://crates.io/crates/sysinfo)
+- [dialoguer](https://crates.io/crates/dialoguer)
+- [rgb](https://crates.io/crates/rgb)
+- [textplots](https://crates.io/crates/textplots)
---
+++ src/main.rs
@@ -0,0 +1,118 @@
+use sysinfo::{Pid, PidExt, ProcessExt, System, SystemExt};
+use std::io::{self};
+use std::process::Command;
+use std::thread;
+use std::time::Duration;
+use dialoguer::{Input, Select};
+use dialoguer::theme::ColorfulTheme;
+use rgb::RGB8;
+use textplots::{Chart, ColorPlot, Shape};
+
+struct ProcessItem {
+    pid: u32,
+    name: String,
+}
+
+fn clear_screen() {
+    if cfg!(windows) {
+        // On Windows, use "cls" to clear the screen
+        Command::new("cmd")
+            .args(&["/c", "cls"])
+            .status()
+            .expect("Failed to clear screen");
+    } else {
+        // On Unix-like systems, use "clear" to clear the screen
+        Command::new("clear")
+            .status()
+            .expect("Failed to clear screen");
+    }
+}
+
+fn main() -> Result<(), io::Error> {
+    let mut system = System::new_all();
+
+    let processes: Vec<ProcessItem> = system.processes()
+        .iter()
+        .map(|(&pid, proc)| {
+            let name = proc.name().to_string();
+            let cmdline = proc.cmd().join(" ").to_string(); // Получаем параметры командной строки
+
+            ProcessItem {
+                pid: pid.as_u32(),
+                name: format!("{} - {}", name, cmdline), // Объединяем имя и параметры командной строки
+            }
+        })
+        .collect();
+
+    let filter: String = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("Введите фильтр для названия процесса")
+        .default("".into())
+        .interact_text()
+        .unwrap();
+
+// Применение фильтра
+    let filtered_processes: Vec<&ProcessItem> = processes.iter()
+        .filter(|proc| proc.name.to_lowercase().contains(&filter.to_lowercase()))
+        .collect();
+
+    if filtered_processes.is_empty() {
+        println!("Нет процессов, соответствующих фильтру.");
+        return Ok(());
+    }
+
+// Подготовка списка строк для интерфейса выбора
+    let selection_items: Vec<String> = filtered_processes
+        .iter()
+        .map(|proc| format!("PID {}: {}", proc.pid, proc.name))
+        .collect();
+
+// Выбор процесса из отфильтрованного списка
+    let selection_index = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Выберите процесс")
+        .default(0)
+        .items(&selection_items)
+        .interact_opt()
+        .unwrap();
+
+    match selection_index {
+        Some(index) => {
+            let selected_process = &filtered_processes[index];
+            println!("Выбран процесс: PID {} - {}", selected_process.pid, selected_process.name);
+            let pid = Pid::from_u32(selected_process.pid);
+
+            let mut cpu_data = vec![]; // Вектор для хранения данных CPU
+            let mut memory_data = vec![]; // Вектор для хранения данных памяти
+
+            loop {
+                system.refresh_process(pid);
+                if let Some(proc) = system.process(pid) {
+                    let cpu_usage = proc.cpu_usage() as f32;
+                    let memory_usage = proc.memory() as f32 / 1024.0 / 1024.0; // Convert to MB
+
+                    cpu_data.push((cpu_data.len() as f32, cpu_usage)); // Добавляем данные CPU как кортежи
+                    memory_data.push((memory_data.len() as f32, memory_usage)); // Добавляем данные памяти как кортежи
+
+                    if cpu_data.len() >= 100 {
+                        cpu_data.clear();
+                        memory_data.clear();
+                    }
+
+                    clear_screen();
+                    println!("\nred = CPU (Usage: {:.2} %), green = Memory (Usage: {:.2} MB)\n", cpu_usage, memory_usage);
+                    let mut chart = Chart::new(180, 40, -1.0, 100.0);
+                    chart.linecolorplot(
+                        &Shape::Lines(&cpu_data),
+                        RGB8::new(255, 0, 0),
+                    ).linecolorplot(
+                        &Shape::Lines(&memory_data),
+                        RGB8::new(0, 255, 0),
+                    ).display();
+                }
+
+                thread::sleep(Duration::from_secs(1));
+            }
+        }
+        None => println!("Выбор отменен."),
+    }
+    Ok(())
+}
